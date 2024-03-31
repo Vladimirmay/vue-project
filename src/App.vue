829b30ab28bc3bc5e6a0628ede7101296e68712b
Vladimir Mayevsky <vmayevskiy3009@gmail.com>
2024-03-31 20:50:09 +0300

    card v1

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,21 @@ const count = ref(0);
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl drop-shadow-xl mt-14">
     <Header />
+
+    <div>
+      <div>
+        <img src="/unliked.svg" alt="Like" />
+        <img src="/sneakers/1.jpg" alt="sneakers" />
+        <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+        <div class="flex justify-between">
+          <div>
+            <span>Цeна:</span>
+            <span>12 999 руб.</span>
+          </div>
+          <img src="/plus.svg" alt="Add" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
