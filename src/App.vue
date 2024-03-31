83ff2f1fb8f26885aadf53ae7c43300b7e56b30d
Vladimir Mayevsky <vmayevskiy3009@gmail.com>
2024-03-31 15:29:02 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+body {
+  background: red;
+}
+button {
+  font-weight: bold;
+}
+</style>
